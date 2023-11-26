3210aa0ce88a751f9014a7989d88630247e79b09
jose hernandez <joseo3266@gmail.com>
2023-11-26 14:24:26 -0700

    Primera vez para git

---
+++ pruebas.py
@@ -0,0 +1,11 @@
+message = "Putos todos"
+
+def fracture(word):
+
+    word_fractured = []
+    for letter in word:
+        word_fractured.append(letter)
+    
+    print(word_fractured)
+
+fracture(message)
